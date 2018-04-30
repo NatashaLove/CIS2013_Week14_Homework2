d680828c9363549a855d04cd893f7eb39bf6f2f7
Natasha S Love <debora@inbox.ru>
2018-04-29 19:11:14 -0500

    Initial commit

---
+++ ToDo.h
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstring>
+#include <string> 
+using namespace std;
+
+// g++ main.cpp ToDo.cpp
+
+class ToDo {
+	private: 
+	string *list; // will be array - list of strings (words);
+	int length =0;
+	int next=0;
+	string item;
+	
+	public:
+	
+	// default constructor
+	ToDo ();
+	
+	//create list of length len
+	ToDo (int len);
+	
+	~ToDo (); // Destroy the list
+	
+	//void add (getline (cin, item)); // add stuff to list
+	void add(string item);
+	void  done (); // finish/deletes the last thing in list
+	void print ();
+	
+	
+	
+};
